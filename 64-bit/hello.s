8ef46f9f138072dcb43cc07aa63ebdabe52f7cb1
kado.kado112 <108268013+kado-kado@users.noreply.github.com>
2025-11-01 21:01:27 +0900

    Compile a 64-bit Hello World in C with GCC

---
+++ 64-bit/hello.s
@@ -0,0 +1,31 @@
+	.file	"hello.c"
+	.text
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Hello World.\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movq	%rdx, 24(%rbp)
+	call	__main
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rcx
+	call	puts
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (MinGW-W64 x86_64-ucrt-posix-seh, built by Brecht Sanders, r1) 15.1.0"
+	.def	puts;	.scl	2;	.type	32;	.endef
